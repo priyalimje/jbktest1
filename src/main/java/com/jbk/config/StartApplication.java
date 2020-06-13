3cbe28f9f54b1285ea6a604f618f588711e04733
Ashish Limje <ashishlimje130887@gmail.com>
2020-06-13 19:55:10 +0530

    FIrst Cmmit

---
+++ src/main/java/com/jbk/config/StartApplication.java
@@ -0,0 +1,14 @@
+package com.jbk.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.jbk")
+public class StartApplication {
+public static void main(String[] args) {
+	SpringApplication.run(StartApplication.class, args);
+	System.out.println("11111111111111");
+}
+}
